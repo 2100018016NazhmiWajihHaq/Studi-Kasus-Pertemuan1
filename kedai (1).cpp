31109b57ac412992901d3d5525d7d7ffb9e503d1
2100018016NazhmiWajihHaq <muhammad2100018016@webmail.uad.ac.id>
2022-03-25 16:14:49 +0700

    Study Kasus

---
+++ kedai (1).cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+main()
+{
+char pembelian;
+int kode, porsi, jumlah, banyak;
+long int total,diskon,akhir, bayar, harga,duit,kembalian;
+atas:
+cout<<" ====================== MENU MAKANAN ======================="<<endl;
+cout<<" 1. Ayam Goreng "<<endl;
+cout<<" 2. Ayam Bakar" <<endl;
+cout<<"============================================================"<<endl;
+cout<<"Jumlah Menu Yang Dipesan = ";
+cin>>banyak;
+jumlah=1;
+bayar=0;
+for(jumlah=1;jumlah<=banyak;jumlah++)
+{ulang:
+cout<<" Masukan Kode Menu = ";
+cin>>kode;
+if (kode==1)
+{
+cout<<" Nama Makanan = Ayam Goreng\n";
+cout<<" Harga        = Rp.17000,-\n"<<endl;
+cout<<" Jumlah Porsi = ";
+cin>>porsi;
+harga=17000;
+total=harga*porsi;
+cout<<" Total Harga  = Rp"<<total<<endl;
+}
+else
+if (kode==2)
+{
+cout<<" Nama Makanan = Ayam Bakar\n";
+cout<<" Harga        = Rp. 21000,-\n"<<endl;
+cout<<" Jumlah Porsi = ";
+cin>>porsi;
+harga=21000;
+total=harga*porsi;
+cout<<" Total Harga 		= Rp"<<total<<endl;
+}
+else
+{
+cout<<"-Invalid Number-a";
+goto ulang;
+}
+bayar=bayar+total;
+}
+cout<<" Total Bayar		= Rp"<<bayar<<",-"<<endl;
+cout<<" Masukan Pembayaran	= Rp";cin>>duit;
+if(bayar>45000){
+diskon = bayar*0.10;
+cout<<"Kamu Dapat Diskon 10%    "<<endl;
+}
+else if(bayar<45000) {
+cout<<"Tidak dapet diskon    "<<endl;
+diskon = 0;
+}
+akhir=bayar-diskon;
+kembalian=duit-akhir;
+cout<<"Kamu Dapat Diskon      = Rp"<<diskon<<",-"<<endl;
+cout<<"Cash Back              = Rp"<<kembalian<<",-"<<endl;
+cout<<"           TERIMA KASIH"<<endl;
+cout<<"Pembelian Baru [y/t] ";cin>>pembelian;
+if (pembelian=='y'||pembelian=='Y')
+goto atas;
+else
+cout<<"Selamat Tinggal";
+}
